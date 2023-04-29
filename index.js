ccb34c9ed6d939683a9f65a4fe48fed7f6ecde17
zulfikar2022 <sayedzulfikar2019@gmail.com>
2023-04-30 00:06:40 +0600

    basic dragon server categories

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+// const port = process.ENV.PORT || 5000;
+const port = 5000;
+
+const categories = require('./data/categories.json') 
+
+app.use(cors());
+app.get('/',(req,res)=> {
+    res.send('Dragon is running')
+});
+
+app.get('/categories',(req,res) => {
+    res.send(categories)
+})
+
+
+
+
+app.listen(port, () => {
+    console.log(`Dragon api is running on port ${port}`);
+})
