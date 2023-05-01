428e450e8c53eace75e706fa0bea7129b1f6f1b7
zulfikar2022 <sayedzulfikar2019@gmail.com>
2023-05-01 10:55:06 +0600

    vercel config file added

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ const cors = require('cors');
 const port = 5000;
 
 const categories = require('./data/categories.json') 
+const news = require('./data/news.json')
 
 app.use(cors());
+
+// ALL GETS HERE
+
 app.get('/',(req,res)=> {
     res.send('Dragon is running')
 });
@@ -15,6 +19,28 @@ app.get('/',(req,res)=> {
 app.get('/categories',(req,res) => {
     res.send(categories)
 })
+
+app.get('/news',(req,res) => {
+    res.send(news);
+})
+
+app.get('/news/:id',(req,res) => {
+    const  id = req.params.id;
+    const selectedNews = news.find(n => n._id === id)
+    res.send(selectedNews);
+})
+
+app.get('/categories/:id',(req,res) => {
+    const id = parseInt(req.params.id);
+    if(id === 0){
+        res.send(news);
+    }
+    else{
+        const categoryNews = news.filter(n => parseInt(n.category_id) === id);
+        res.send(categoryNews);
+    }
+})
+
 
 
 
